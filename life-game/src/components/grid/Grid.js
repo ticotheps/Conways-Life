e432067da87337a3450d35b1d078384d10d03d5f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-15 18:32:22 -0400

    Creates an onClick handler and a ref attribute on the div.Board element

--- life-game/src/components/grid/Grid.js
+++ life-game/src/components/grid/Grid.js
@@ -7,11 +7,57 @@ const WIDTH = 500;
 const HEIGHT = 500;
 
 class Grid extends Component {
+  rows = HEIGHT / CELL_SIZE;
+  cols = WIDTH / CELL_SIZE;
+  board = this.makeEmptyBoard();
+
+  state = {
+    cells: [],
+  };
+
+  // Creates an empty board
+  makeEmptyBoard() {
+    let board = [];
+
+    for (let y = 0; y < this.rows; y++) {
+      board[y] = [];
+      for (let x = 0; x < this.cols; x++) {
+        board[y][x] = false;
+      }
+    }
+    // console.log(board);
+    return board;
+  }
+
+  // Create cells from this.board
+  makeCells() {
+    let cells = [];
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (this.board[y][x]) {
+          cells.push({ x, y });
+        }
+      }
+    }
+    return cells;
+  }
+
   render() {
     return (
       <div>
-        <h2>Testing</h2>
-        <div className='Board' style={{ width: WIDTH, height: HEIGHT }} />
+        <div
+          className='Board'
+          style={{
+            width: WIDTH,
+            height: HEIGHT,
+            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
+          }}
+          onClick={this.handleClick}
+          ref={n => {
+            this.boardRef = n;
+          }}
+        />
       </div>
     );
   }
