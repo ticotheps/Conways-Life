051cc814378c217a13788e4ea4fbfdaf50a76f5d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-19 14:03:10 -0400

    Updates 'About' section

--- life-game/src/components/about/About.js
+++ life-game/src/components/about/About.js
@@ -2,13 +2,45 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
+    <div className='about-container'>
       <br />
       <h2>What is "Conway's Game of Life"?</h2>
       <h3 className='polyas-heading'>Understanding the Problem</h3>
+      <p className='polyas-content'>
+        A mathematician named John Conway created this zero-player game in 1970
+        to create a model that enabled him to study "cellular automata" (the
+        plural form of "cellular automaton"). A cellular automaton is a
+        collection of cells on a grid that evolve over "generations" (a unit of
+        time measurement in the game) according to rules based on the "states"
+        ("alive" or "dead") neighboring cells.
+      </p>
       <h3 className='polyas-heading'>Devising a Plan</h3>
+      <p className='polyas-content'>
+        A mathematician named John Conway created this zero-player game in 1970
+        to create a model that enabled him to study "cellular automata" (the
+        plural form of "cellular automaton"). A cellular automaton is a
+        collection of cells on a grid that evolve over "generations" (a unit of
+        time measurement in the game) according to rules based on the "states"
+        ("alive" or "dead") neighboring cells.
+      </p>
       <h3 className='polyas-heading'>Implementing the Plan</h3>
+      <p className='polyas-content'>
+        A mathematician named John Conway created this zero-player game in 1970
+        to create a model that enabled him to study "cellular automata" (the
+        plural form of "cellular automaton"). A cellular automaton is a
+        collection of cells on a grid that evolve over "generations" (a unit of
+        time measurement in the game) according to rules based on the "states"
+        ("alive" or "dead") neighboring cells.
+      </p>
       <h3 className='polyas-heading'>Reflecting/Iterating</h3>
+      <p className='polyas-content'>
+        A mathematician named John Conway created this zero-player game in 1970
+        to create a model that enabled him to study "cellular automata" (the
+        plural form of "cellular automaton"). A cellular automaton is a
+        collection of cells on a grid that evolve over "generations" (a unit of
+        time measurement in the game) according to rules based on the "states"
+        ("alive" or "dead") neighboring cells.
+      </p>
     </div>
   );
 };
