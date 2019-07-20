96ed7efff6ec2bbf2acb6538f0ce8c6b88fae0b4
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-20 14:16:55 -0400

    Adds photo of author to <footer>

--- life-game/src/App.js
+++ life-game/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Grid from './components/grid/Grid';
 import Rules from './components/rules/Rules';
 import About from './components/about/About';
+import tico from './tico.jpg';
 
 import './App.css';
 
@@ -13,19 +14,35 @@ function App() {
         <Grid />
         <About />
       </div>
-      <Rules />
-      <p className='footer'>© Tico S. Thepsourinthone 2019</p>
-      <p className='footer'>
-        See more of Tico's Work at{' '}
-        <a
-          href='http://heytico.com'
-          target='_blank'
-          className='footer-plug'
-          rel='noopener noreferrer'
-        >
-          heytico.com
-        </a>
-      </p>
+      <div className='rules-footer-container'>
+        <Rules />
+        <div className='footer-container'>
+          <div className='author-container'>
+            <a
+              href='https://www.linkedin.com/in/ticotheps/'
+              target='_blank'
+              className='author-pic-link'
+              rel='noopener noreferrer'
+            >
+              <img className='author-pic' src={tico} alt='author headshot' />
+            </a>
+          </div>
+          <div>
+            <p className='footer'>© Tico S. Thepsourinthone 2019</p>
+            <p className='footer'>
+              See more of Tico's Work at{' '}
+              <a
+                href='http://heytico.com'
+                target='_blank'
+                className='footer-plug'
+                rel='noopener noreferrer'
+              >
+                heytico.com
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
